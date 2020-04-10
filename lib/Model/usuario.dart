7421228f33115132b4d3648cb790420d0ba12dd6
Kauê Murakami <kauetmurakami@gmail.com>
2020-04-09 21:23:52 -0300

    criando interface de mensagem, caixa de mensagem e botao

--- lib/Model/usuario.dart
+++ lib/Model/usuario.dart
@@ -1,16 +1,12 @@
 
 
+import 'package:flutter/cupertino.dart';
+
 class Usuario{
    String _nome;
    String _email;
    String _senha;
    String _urlImagem;
-
-   String get nome => _nome;
-
-   set nome(String value) {
-     _nome = value;
-   }
 
    Map<String, dynamic> toMap(usaurio){
       Map<String,dynamic> map = {
@@ -20,21 +16,32 @@ class Usuario{
 
       return map;
    }
+   @protected
+   String get nome => _nome;
 
+   @protected
+   set nome(String value) {
+     _nome = value;
+   }
+
+   @protected
    String get email => _email;
 
+   @protected
    String get urlImagem => _urlImagem;
 
+   @protected
    set urlImagem(String value) {
      _urlImagem = value;
    }
-
+   @protected
    String get senha => _senha;
 
+   @protected
    set senha(String value) {
      _senha = value;
    }
-
+   @protected
    set email(String value) {
      _email = value;
    }
--- lib/mensagens.dart
+++ lib/mensagens.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'Model/usuario.dart';
@@ -15,11 +16,82 @@ class Mensagem extends StatefulWidget {
 class _MensagemState extends State<Mensagem> {
   @override
   Widget build(BuildContext context) {
+
+    _enviarFoto(){
+
+    }
+    _enviarMensagem(){
+
+    }
+
+    TextEditingController _controllerMensagem = TextEditingController();
+
+    var caixaMensagem = Container(
+      padding: EdgeInsets.all(8),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+                padding: EdgeInsets.only(right: 8),
+              child: TextField(
+                controller: _controllerMensagem,
+                keyboardType: TextInputType.text,
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20),
+                decoration: InputDecoration(
+                  hintText: "Digite uma mensagem...",
+                  contentPadding: EdgeInsets.fromLTRB(16, 8, 16, 8),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
+                  prefixIcon: IconButton(
+                    icon: Icon(Icons.camera_alt),
+                    onPressed: _enviarFoto(),
+                  )
+                ),
+              ),
+            ),
+          ),
+          FloatingActionButton(
+            backgroundColor: Color(0xff075E54),
+            child: Icon(
+              Icons.send, color: Colors.white,
+            ),
+            mini: true,
+            onPressed: _enviarMensagem(),
+          )
+        ],
+      ),
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.contato.nome),
       ),
-      body: Container(),
+      body: Container(
+        child: SafeArea(
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            padding: EdgeInsets.all(16),
+            child: Column(
+              children: <Widget>[
+                Text("x"),
+                caixaMensagem,
+              ],
+            ),
+          )
+        ),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("images/bg.png"),
+            fit: BoxFit.cover
+          )
+        ),
+      ),
 
     );
   }
